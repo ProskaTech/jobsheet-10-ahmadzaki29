8e14279ccd906bf1a9aed69c624a71bbf219dc5a
ahmad zaki alfaruq- 21343079 <112170461+ahmadzaki29@users.noreply.github.com>
2022-11-03 20:39:14 +0700

    Add files via upload

---
+++ tugas/smartphone/andromain.java
@@ -0,0 +1,15 @@
+package smartphone;
+
+/**
+ *
+ * Created by Ahmad Zaki Alfaruq_21343079
+ */
+public class andromain {
+    public static void main(String args[]){
+        android oppo = new android();
+        oppo.nyala();
+        oppo.panggilan();
+        oppo.sms();
+        oppo.shutdown();
+    }
+}
